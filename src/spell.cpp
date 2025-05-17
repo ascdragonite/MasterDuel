74cf093f10f3c3e0f9b5b96094b723b535c45518
kuhakuhogan <kuhakuhogan@gmail.com>
2025-05-17 20:25:41 +0700

    thêm lá Dragon United, Destroyer, fix Dark Magic

--- src/spell.cpp
+++ src/spell.cpp
@@ -19,49 +19,41 @@ bool OshamaScramble::ActivateEffect(Player &self,
 
     return true; // Indicate success
 }
-/*
+
 
 bool DarkMagic::ActivateEffect(Player& self, Player& opponent) {
-
-    //logic card
-    //Step 2: Implement the logic of the card here
-
-
-    if (name == "Dark Magic") {
-        // B1: Kiểm tra xem player có điều khiển "Dark Magician" không
-        bool hasDarkMagician = false;
-        for (Card* c : self.getField()) {
-            if (MonsterCard*m = dynamic_cast<MonsterCard*>(c)) {
-                if (m->getName() == "Dark Magician") {
-                    hasDarkMagician = true;
-                    break;
-                }
+    int countm = 0;
+    vector<Card*> newfield1 = self.getField();
+    vector<Card*> newfield2 = opponent.getField();
+    vector<Card*> newfieldopp;
+    for(auto card1 : newfield1){
+        if(card1->getName() == "Dark Magician"){
+            countm++;
+    }
+    }
+    if(countm == 0){
+        cout << "[Dark Magic] Activation failed: You do not control a Dark Magician" << endl;
+        return false;
+    }
+    if(countm > 0){
+        for(auto card2 : newfield2){
+            if(card2->getType()!="Monster"){
+                newfieldopp.push_back(card2);
             }
-        }
-
-        if (!hasDarkMagician) {
-            cout << "[Dark Magic] Activation failed: You do not control a Dark
-Magician.\n"; return false;
-        }
-
-        // B2: Tiêu diệt toàn bộ monster bên opponent
-        vector<Card*> newField;
-        for (Card* c : opponent.getField()) {
-            if (c->getType() != "Monster") {
-                newField.push_back(c); // Giữ lại các lá khác
-            } else {
-                if (c != nullptr) {
-                    cout << "[Dark Magic] Destroyed: " << c->getName() << endl;
-                    delete c;
-                }
+            if(card2->getType()=="Monster"){
+                cout << "[Dark Magic] Destroyed: " << card2->getName() << endl;
             }
+                
         }
-
-        opponent.setField(newField); // Cập nhật lại field
+        cout << "[Dark Magic] Activate successfully! All opponent's monster cards are destroyed" << endl;
+        opponent.setField(newfieldopp);
     }
 
-    return true; // Indicate success
-} */
+return true; // Indicate success
+}
+/*
+
+
 
 bool ReEndOfADream::ActivateEffect(Player &self,
                                    Player &opponent) { // thêm lượt nếu hp<1000
@@ -111,8 +103,7 @@ bool WorldVanquisher::ActivateEffect(Player &self,
         }
     }
     if (countC == 0) {
-        cout << "[World Vanquisher] Activation Failed : You need at least 1 "
-            "monster card"
+        cout << "[World Vanquisher] Activation Failed : You need at least 1 monster card"
             << endl;
         return false;
     }
@@ -122,15 +113,12 @@ bool WorldVanquisher::ActivateEffect(Player &self,
             cout << "[World Vanquisher] : Choose the card you want to buff" << endl;
             cin >> in;
             if (newfield[in]->getType() != "Monster") {
-                cout << "[World Vanquisher] Activation Failed : You need to choose a "
-                    "monster card"
-                    << endl;
+                cout << "[World Vanquisher] Activation Failed : You need to choose a monster card" << endl;
             }
         } while (newfield[in]->getType() != "Monster");
         MonsterCard *card = dynamic_cast<MonsterCard *>(newfield[in]);
         card->setAtk(card->getAtk() + 200);
-        cout << "[World Vanquisher] successfully gained " << card->getName()
-            << " 200 atk" << endl;
+        cout << "[World Vanquisher] successfully gained " << card->getName() << " 200 atk" << endl;
     }
     return true; // Indicate success
 }
@@ -153,9 +141,7 @@ bool FlowerSnowDrumNBass::ActivateEffect(Player &self, Player &opponent) {
     r = rand() % 2 + 1;
     if (r == 1) {
         if (cself < 2) {
-            cout << "[Flower Snow Drum n Bass] Activation Failed : There is not "
-                "enough card in your deck"
-                << endl;
+            cout << "[Flower Snow Drum n Bass] Activation Failed : There is not enough card in your deck"<< endl;
             return false;
         }
         self.drawCard();
@@ -164,8 +150,7 @@ bool FlowerSnowDrumNBass::ActivateEffect(Player &self, Player &opponent) {
     }
     if (r == 2) {
         if (copp < 2) {
-            cout << "[Flower Snow Drum n Bass] Activation Failed : There is not "
-                "enough card in opponent's deck"
+            cout << "[Flower Snow Drum n Bass] Activation Failed : There is not enough card in opponent's deck"
                 << endl;
             return false;
         }
@@ -174,5 +159,78 @@ bool FlowerSnowDrumNBass::ActivateEffect(Player &self, Player &opponent) {
         cout << "[Flower Snow Drum n Bass] successfully drew your opponent 2 cards"
             << endl;
     }
+    return true; // Indicate success
+}
+bool DragonUnited::ActivateEffect(Player &self, Player &opponent) {
+        vector<Card *> newfield = self.getField();
+    int countC = 0;
+    for (auto card : newfield) {
+
+        if (card->getType() == "Monster") {
+            countC++;
+        }
+    }
+    if (countC == 0) {
+        cout << "[Dragon United] Activation Failed : You need at least 1 monster card" << endl;
+        return false;
+    }
+    if (countC > 0) {
+        for(auto card : newfield){
+            if (card->getType() == "Monster"){
+                MonsterCard *card1 = dynamic_cast<MonsterCard *>(card);
+                if(card1 != nullptr &&card1->isFacedown()==false){
+                card1->setAtk(card1->getAtk() + 100);
+            cout << "[Dragon United] successfully gained " << card1->getName() << " 100 atk" << endl;
+                }
+    }
+
+            }
+        }
+    return true; // Indicate success
+}
+
+bool Destroyer::ActivateEffect(Player& self, Player& opponent) {
+ vector<Card *> newfield = opponent.getField();
+ vector<Card*> newfieldopp;
+    int countC = 0;
+    for (auto card : newfield) {
+
+        if (card->getType() == "Monster") {
+            countC++;
+        }
+    }
+    if (countC == 0) {
+        cout << "[Destroyer] Activation Failed : Opponent need to have at least 1 monster card"
+            << endl;
+        return false;
+    }
+    if (countC > 0) {
+        int in;
+        MonsterCard* card1 = nullptr;
+        do {
+            cout << "[Destroyer] : Choose the card you want to destroy" << endl;
+            cin >> in;
+
+            
+            if (in < 0 || in >= newfield.size()) {
+                cout << "[Destroyer] Invalid index!" << endl;
+                card1 = nullptr;
+                continue;  // ⬅ Tại sao?
+            }
+
+            card1 = dynamic_cast<MonsterCard*>(newfield[in]);
+
+            if (!card1 || card1->getType() != "Monster"||card1->isFacedown()==false) {
+                cout << "[Destroyer] Activation Failed : You need to choose a defense monster card" << endl;
+            }
+        } while (!card1 || card1->getType() != "Monster"||card1->isFacedown()==false);
+        for(auto card0 : newfield){
+            if(card0 != newfield[in]){
+                newfieldopp.push_back(card0);
+            }
+            if(card0 == newfield[in]){
+                cout << "[Destroyer] successfully destroy 1 defense card : " << card0->getName() << endl;
+            }
+        }
     return true; // Indicate success
 }
